b94cd32d2c0ecb59b07564092c177f5fd48b360f
gijuku2025 <chetty-jiovaan@gijuku.ac.jp>
2026-02-05 09:46:00 +0900

    Update script.js

--- civics/srsver1/script.js
+++ civics/srsver1/script.js
@@ -1,6 +1,6 @@
 const app = document.getElementById("app");
 
-const CHAPTER_FILES = ["chapter1", "chapter2", "chapter3", "chapter4", "chapter5", "chapter6"]; // add more later
+const CHAPTER_FILES = ["chapter1", "chapter2", "chapter3", "chapter4", "chapter5", "chapter6", "chapter7"]; // add more later
 const MAX_NEW_PER_DAY = 10;
 
 let state = {
